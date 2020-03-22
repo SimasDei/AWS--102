920c20dec45349053b3696434709692e8291cee0
Simas <simas@simas-dei.com>
2020-03-22 17:02:59 +0200

    add markup for invoice table

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Table, Button } from 'reactstrap';
+import {} from '@fortawesome/free-solid-svg-icons';
+
+const data = [
+  {
+    id: '10pihsdioupghioJGUIOj0',
+    Vendor: 'Nike',
+    Amount: 23000,
+    Date: '2020-01-14',
+  },
+  {
+    id: 'iujiosgjioaGJoi',
+    Vendor: 'Prada',
+    Amount: 42000,
+    Date: '2020-02-12',
+  },
+];
 
 const App = () => {
+  const [invoices, setInvoices] = useState(data);
+  const [loading, setLoading] = useState(false);
+
+  if (loading) return <div>Loading...</div>;
+
+  const removeInvoice = id => {
+    setInvoices(invoices.filter(invoice => invoice.id !== id));
+  };
+
   return (
-    <div>
-      <h1>App o/</h1>
+    <div className='container border border-secondary rounded center text-white'>
+      <div className='row'>
+        <div className='col-xs-12'>
+          <h4>Pending Invoices</h4>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-xs-12 center text-center'>
+          <Table>
+            <thead className='text-white'>
+              <th>Vendor</th>
+              <th>Amount</th>
+              <th>Invoice #</th>
+              <th>Date</th>
+              <th colSpan='4'>Actions</th>
+              <th>Image</th>
+            </thead>
+            <tbody>
+              {invoices.map(invoice => (
+                <tr key={invoice.id} className='text-white'>
+                  <td>{invoice.Vendor}</td>
+                  <td>{invoice.Amount}</td>
+                  <td>{invoice.id}</td>
+                  <td>{invoice.Date}</td>
+                  <td>
+                    <Button className='btn btn-lg btn-success'>Ok</Button>
+                  </td>
+                  <td>
+                    <Button
+                      className='btn btn-lg btn-danger'
+                      onClick={() => removeInvoice(invoice.id)}
+                    >
+                      Nope
+                    </Button>
+                  </td>
+                  <td>
+                    <Button className='btn btn-lg btn-info'>Meh</Button>
+                  </td>
+                  <td>
+                    <Button className='btn btn-lg btn-warning'>??</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      </div>
     </div>
   );
 };
